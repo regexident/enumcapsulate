74c66a83fa901a41b01a832222a7fd3403b9d55a
Vincent Esche <regexident@gmail.com>
2024-01-16 17:12:48 +0100

    Add derive proc-macros via 'enumcapsulate-macros' crate

---
+++ enumcapsulate-macros/src/lib.rs
@@ -0,0 +1,3 @@
+use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
+use syn::{parse_macro_input, DeriveInput};
--- enumcapsulate/src/lib.rs
+++ enumcapsulate/src/lib.rs
@@ -1,36 +1,5 @@
+#[cfg(feature = "macros")]
+pub use enumcapsulate_macros as macros;
+
 #[cfg(feature = "derive")]
-pub use enumcapsulate_macros as derive;
-
-pub trait AsVariantRef<T> {
-    fn as_variant_ref(&self) -> Option<&T>;
-}
-
-pub trait AsVariantMut<T> {
-    fn as_variant_mut(&mut self) -> Option<&mut T>;
-}
-
-pub trait AsVariant<T> {
-    fn as_variant(&self) -> Option<T>;
-}
-
-pub trait IntoVariant<T>: Sized {
-    fn into_variant(self) -> Result<T, Self>;
-}
-
-pub trait Downcast {
-    fn as_downcast_ref<T>(&self) -> Option<&T>
-    where
-        Self: AsVariantRef<T>;
-
-    fn as_downcast_mut<T>(&mut self) -> Option<&mut T>
-    where
-        Self: AsVariantMut<T>;
-
-    fn as_downcast<T>(&self) -> Option<T>
-    where
-        Self: AsVariant<T>;
-
-    fn into_downcast<T>(self) -> Result<T, Self>
-    where
-        Self: IntoVariant<T>;
-}
+pub mod derive {}
