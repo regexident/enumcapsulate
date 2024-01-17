4bf9780c8191125a86897e43bc537b10f46b223c
Vincent Esche <regexident@gmail.com>
2024-01-17 15:37:01 +0100

    Split `tryexpand`-based tests up into more granular units to be able to run them more selectively

--- enumcapsulate/tests/derive-tests.rs
+++ enumcapsulate/tests/derive-tests.rs
@@ -1,43 +1,115 @@
-#[test]
-pub fn pass() {
-    tryexpand::expand_checking([
-        // From
-        "tests/expand-macros/from/pass/**/*.rs",
-        // TryInto
-        "tests/expand-macros/try_into/pass/**/*.rs",
-        // FromVariant
-        "tests/expand-macros/from_variant/pass/**/*.rs",
-        // AsVariantRef
-        "tests/expand-macros/as_variant_ref/pass/**/*.rs",
-        // AsVariantMut
-        "tests/expand-macros/as_variant_mut/pass/**/*.rs",
-        // AsVariant
-        "tests/expand-macros/as_variant/pass/**/*.rs",
-        // IntoVariant
-        "tests/expand-macros/into_variant/pass/**/*.rs",
-        // VariantDowncast
-        "tests/expand-macros/variant_downcast/pass/**/*.rs",
-        // Encapsulate
-        "tests/expand-macros/encapsulate/pass/**/*.rs",
-    ]);
+mod from {
+    #[test]
+    pub fn pass() {
+        tryexpand::expand_checking(["tests/expand-macros/from/pass/**/*.rs"]);
+    }
+
+    #[test]
+    pub fn fail() {
+        tryexpand::expand_fail(["tests/expand-macros/from/fail/**/*.rs"]);
+    }
 }
 
-#[test]
-pub fn fail() {
-    tryexpand::expand_fail([
-        // From
-        "tests/expand-macros/from/fail/**/*.rs",
-        // TryInto
-        "tests/expand-macros/try_into/fail/**/*.rs",
-        // FromVariant
-        "tests/expand-macros/from_variant/fail/**/*.rs",
-        // AsVariantRef
-        "tests/expand-macros/as_variant_ref/fail/**/*.rs",
-        // AsVariantMut
-        "tests/expand-macros/as_variant_mut/fail/**/*.rs",
-        // IntoVariant
-        "tests/expand-macros/into_variant/fail/**/*.rs",
-        // VariantDowncast
-        "tests/expand-macros/variant_downcast/fail/**/*.rs",
-    ]);
+mod try_into {
+    #[test]
+    pub fn pass() {
+        tryexpand::expand_checking(["tests/expand-macros/try_into/pass/**/*.rs"]);
+    }
+
+    #[test]
+    pub fn fail() {
+        tryexpand::expand_fail(["tests/expand-macros/try_into/fail/**/*.rs"]);
+    }
+}
+
+mod from_variant {
+    #[test]
+    pub fn pass() {
+        tryexpand::expand_checking(["tests/expand-macros/from_variant/pass/**/*.rs"]);
+    }
+
+    #[test]
+    pub fn fail() {
+        tryexpand::expand_fail(["tests/expand-macros/from_variant/fail/**/*.rs"]);
+    }
+}
+
+mod as_variant_ref {
+    #[test]
+    pub fn pass() {
+        tryexpand::expand_checking(["tests/expand-macros/as_variant_ref/pass/**/*.rs"]);
+    }
+
+    #[test]
+    pub fn fail() {
+        tryexpand::expand_fail(["tests/expand-macros/as_variant_ref/fail/**/*.rs"]);
+    }
+}
+
+mod as_variant_mut {
+    #[test]
+    pub fn pass() {
+        tryexpand::expand_checking(["tests/expand-macros/as_variant_mut/pass/**/*.rs"]);
+    }
+
+    #[test]
+    pub fn fail() {
+        tryexpand::expand_fail(["tests/expand-macros/as_variant_mut/fail/**/*.rs"]);
+    }
+}
+
+mod as_variant {
+    #[test]
+    pub fn pass() {
+        tryexpand::expand_checking(["tests/expand-macros/as_variant/pass/**/*.rs"]);
+    }
+
+    #[test]
+    pub fn fail() {
+        // the failures are already covered by the tests of the individual
+        // derives that this umbrella derive delegates to.
+        //
+        // As such we only have to make sure in `pass()` that
+        // it does actually derive what it says on the tin.
+    }
+}
+
+mod into_variant {
+    #[test]
+    pub fn pass() {
+        tryexpand::expand_checking(["tests/expand-macros/into_variant/pass/**/*.rs"]);
+    }
+
+    #[test]
+    pub fn fail() {
+        tryexpand::expand_fail(["tests/expand-macros/into_variant/fail/**/*.rs"]);
+    }
+}
+
+mod variant_downcast {
+    #[test]
+    pub fn pass() {
+        tryexpand::expand_checking(["tests/expand-macros/variant_downcast/pass/**/*.rs"]);
+    }
+
+    #[test]
+    pub fn fail() {
+        tryexpand::expand_fail(["tests/expand-macros/variant_downcast/fail/**/*.rs"]);
+    }
+}
+
+mod encapsulate {
+    #[test]
+    pub fn pass() {
+        tryexpand::expand_checking(["tests/expand-macros/encapsulate/pass/**/*.rs"]);
+    }
+
+    #[test]
+    pub fn fail() {
+        // the failures are already covered by the tests of the individual
+        // derives that this umbrella derive delegates to.
+        //
+        // As such we only have to make sure in `pass()` that
+        // it does actually derive what it says on the tin.
+    }
 }
