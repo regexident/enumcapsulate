b84fcf1e11d7ade2aba57fe376d841750522153f
Vincent Esche <regexident@gmail.com>
2024-01-22 15:15:45 +0100

    Add documentation

--- enumcapsulate-macros/src/lib.rs
+++ enumcapsulate-macros/src/lib.rs
@@ -8,6 +8,29 @@ use self::enum_deriver::EnumDeriver;
 mod enum_deriver;
 mod utils;
 
+/// Derive macro generating an impl of the trait `From<T>`.
+///
+/// It generates an impl for each of the enum's
+/// newtype variants, where `Inner` is the variant's field type
+/// and `Outer` is the enclosing enum's type.
+///
+/// ```
+/// struct Inner;
+///
+/// enum Outer {
+///     Inner(Inner),
+///     // ...
+/// }
+///
+/// impl From<Inner> for Outer {
+///     fn from(inner: Inner) -> Self {
+///         Outer::Inner(inner)
+///     }
+/// }
+///
+/// // ...
+/// ```
+///
 #[proc_macro_derive(From)]
 pub fn derive_from(input: TokenStream) -> TokenStream {
     let input: DeriveInput = parse_macro_input!(input);
@@ -18,6 +41,38 @@ pub fn derive_from(input: TokenStream) -> TokenStream {
     })
 }
 
+/// Derive macro generating an impl of the trait `TryFrom<T>`.
+///
+/// It generates an impl for each of the enum's newtype variants,
+/// where `Inner` is the variant's field type and `Outer`
+/// is the enclosing enum's type.
+///
+/// ```
+/// struct Inner;
+///
+/// enum Outer {
+///     Inner(Inner),
+///     // ...
+/// }
+///
+/// impl TryFrom<Outer> for Inner {
+///     type Error = Outer;
+///
+///     fn try_from(outer: Outer) -> Result<Self, Self::Error> {
+///         match outer {
+///             Outer::Inner(inner) => Ok(inner),
+///             err => Err(err),
+///         }
+///     }
+/// }
+///
+/// // ...
+/// ```
+///
+/// Note: Despite the derive macro's name it's actually `TryFrom<T>`
+/// that's being derives, not `TryInto<T>`.
+/// But since the macro is derived on the enum the latter feels
+/// more appropriate as the derive's name.
 #[proc_macro_derive(TryInto)]
 pub fn derive_try_from(input: TokenStream) -> TokenStream {
     let input: DeriveInput = parse_macro_input!(input);
@@ -28,6 +83,31 @@ pub fn derive_try_from(input: TokenStream) -> TokenStream {
     })
 }
 
+/// Derive macro generating an impl of the trait `FromVariant<T>`.
+///
+/// It generates an impl for each of the enum's
+/// newtype variants, where `Inner` is the variant's field type
+/// and `Outer` is the enclosing enum's type.
+///
+/// ```
+/// # use enumcapsulate::FromVariant;
+/// #
+/// # struct Inner;
+///
+/// enum Outer {
+///     Inner(Inner),
+///     // ...
+/// }
+///
+/// impl FromVariant<Inner> for Outer {
+///     fn from_variant(inner: Inner) -> Self {
+///         Outer::Inner(inner)
+///     }
+/// }
+///
+/// // ...
+/// ```
+///
 #[proc_macro_derive(FromVariant)]
 pub fn derive_from_variant(input: TokenStream) -> TokenStream {
     let input: DeriveInput = parse_macro_input!(input);
@@ -38,6 +118,34 @@ pub fn derive_from_variant(input: TokenStream) -> TokenStream {
     })
 }
 
+/// Derive macro generating an impl of the trait `FromVariant<T>`.
+///
+/// It generates an impl for each of the enum's
+/// newtype variants, where `Inner` is the variant's field type
+/// and `Outer` is the enclosing enum's type.
+///
+/// ```
+/// # use enumcapsulate::AsVariantRef;
+///
+/// struct Inner;
+///
+/// enum Outer {
+///     Inner(Inner),
+///     // ...
+/// }
+///
+/// impl AsVariantRef<Inner> for Outer {
+///     fn as_variant_ref(&self) -> Option<&Inner> {
+///         match self {
+///             Outer::Inner(inner) => Some(inner),
+///             _ => None,
+///         }
+///     }
+/// }
+///
+/// // ...
+/// ```
+///
 #[proc_macro_derive(AsVariantRef)]
 pub fn derive_as_variant_ref(input: TokenStream) -> TokenStream {
     let input: DeriveInput = parse_macro_input!(input);
@@ -48,6 +156,34 @@ pub fn derive_as_variant_ref(input: TokenStream) -> TokenStream {
     })
 }
 
+/// Derive macro generating an impl of the trait `From<T>`.
+///
+/// It generates an impl for each of the enum's
+/// newtype variants, where `Inner` is the variant's field type
+/// and `Outer` is the enclosing enum's type.
+///
+/// ```
+/// # use enumcapsulate::AsVariantMut;
+///
+/// struct Inner;
+///
+/// enum Outer {
+///     Inner(Inner),
+///     // ...
+/// }
+///
+/// impl AsVariantMut<Inner> for Outer {
+///     fn as_variant_mut(&mut self) -> Option<&mut Inner> {
+///         match self {
+///             Outer::Inner(inner) => Some(inner),
+///             _ => None,
+///         }
+///     }
+/// }
+///
+/// // ...
+/// ```
+///
 #[proc_macro_derive(AsVariantMut)]
 pub fn derive_as_variant_mut(input: TokenStream) -> TokenStream {
     let input: DeriveInput = parse_macro_input!(input);
@@ -58,6 +194,35 @@ pub fn derive_as_variant_mut(input: TokenStream) -> TokenStream {
     })
 }
 
+/// Derive macro generating an impl of the trait `AsVariant<T>`.
+///
+/// It generates an impl for each of the enum's
+/// newtype variants, where `Inner` is the variant's field type
+/// and `Outer` is the enclosing enum's type.
+///
+/// ```
+/// # use enumcapsulate::AsVariant;
+///
+/// #[derive(Clone)]
+/// struct Inner;
+///
+/// enum Outer {
+///     Inner(Inner),
+///     // ...
+/// }
+///
+/// impl AsVariant<Inner> for Outer {
+///     fn as_variant(&self) -> Option<Inner> {
+///          match self {
+///             Outer::Inner(inner) => Some(inner.clone()),
+///             _ => None,
+///         }
+///     }
+/// }
+///
+/// // ...
+/// ```
+///
 #[proc_macro_derive(AsVariant)]
 pub fn derive_as_variant(input: TokenStream) -> TokenStream {
     let input: DeriveInput = parse_macro_input!(input);
@@ -75,6 +240,38 @@ pub fn derive_as_variant(input: TokenStream) -> TokenStream {
     })
 }
 
+/// Derive macro generating an impl of the trait `IntoVariant<T>`.
+///
+/// It generates an impl for each of the enum's
+/// newtype variants, where `Inner` is the variant's field type
+/// and `Outer` is the enclosing enum's type.
+///
+/// ```
+/// # use enumcapsulate::IntoVariant;
+///
+/// struct Inner;
+///
+/// enum Outer {
+///     Inner(Inner),
+///     // ...
+/// }
+///
+/// impl IntoVariant<Inner> for Outer {
+///     fn into_variant(self) -> Result<T, Self> {
+///         match self {
+///             Outer::Inner(inner) => Ok(inner),
+///             err => Err(err),
+///         }
+///     }
+/// }
+///
+/// // ...
+/// ```
+///
+/// Note: Despite the derive macro's name it's actually `TryFrom<T>`
+/// that's being derives, not `TryInto<T>`.
+/// But since the macro is derived on the enum the latter feels
+/// more appropriate as the derive's name.
 #[proc_macro_derive(IntoVariant)]
 pub fn derive_into_variant(input: TokenStream) -> TokenStream {
     let input: DeriveInput = parse_macro_input!(input);
@@ -85,6 +282,21 @@ pub fn derive_into_variant(input: TokenStream) -> TokenStream {
     })
 }
 
+/// Derive macro generating an impl of the trait `VariantDowncast`.
+///
+/// ```
+/// struct Inner;
+///
+/// enum Outer {
+///     Inner(Inner),
+///     // ...
+/// }
+///
+/// // The generated impl looks something along these lines:
+///
+/// impl VariantDowncast for Outer {}
+/// ```
+///
 #[proc_macro_derive(VariantDowncast)]
 pub fn derive_variant_downcast(input: TokenStream) -> TokenStream {
     let input: DeriveInput = parse_macro_input!(input);
@@ -94,6 +306,26 @@ pub fn derive_variant_downcast(input: TokenStream) -> TokenStream {
         deriver.derive_variant_downcast()
     })
 }
+
+/// Umbrella derive macro.
+///
+/// `#[derive(Encapsulate)` is equivalent to the following:
+///
+/// ```
+/// # use enumcapsulate::{
+/// #     derive::{
+/// #         From, TryInto, FromVariant, AsVariantRef, AsVariantMut, IntoVariant, VariantDowncast
+/// #     },
+/// # };
+///
+/// struct Inner;
+///
+/// #[derive(From, TryInto, FromVariant, AsVariantRef, AsVariantMut, IntoVariant, VariantDowncast)]
+/// enum Outer {
+///     Inner(Inner),
+///     // ...
+/// }
+/// ```
 #[proc_macro_derive(Encapsulate)]
 pub fn derive_encapsulate(input: TokenStream) -> TokenStream {
     let input: DeriveInput = parse_macro_input!(input);
--- enumcapsulate/src/lib.rs
+++ enumcapsulate/src/lib.rs
@@ -1,6 +1,10 @@
+//! Traits and corresponding derive macros for enum-based encapsulation.
+
+/// Macros.
 #[cfg(feature = "macros")]
 pub use enumcapsulate_macros as macros;
 
+/// Derive macros.
 #[cfg(feature = "derive")]
 pub mod derive {
     pub use super::macros::{
@@ -8,13 +12,20 @@ pub mod derive {
     };
 }
 
+/// Used to do variant-to-enum conversions
+/// between an outer enum's and its inner variant's type.
 pub trait FromVariant<T> {
+    /// Converts to this type from the variant's type.
     fn from_variant(variant: T) -> Self;
 }
 
+/// Used to do a cheap reference-to-reference reference conversion
+/// between an outer enum's and its inner variant's type.
 pub trait AsVariantRef<T> {
+    /// Returns some reference to the inner value if it is of type `T`, or `None`` if it isn’t.
     fn as_variant_ref(&self) -> Option<&T>;
 
+    /// Returns a copy of the inner value if it is of type `T`, or `None`` if it isn’t.
     fn as_variant(&self) -> Option<T>
     where
         T: Clone,
@@ -23,15 +34,30 @@ pub trait AsVariantRef<T> {
     }
 }
 
+/// Used to do a cheap mutable-to-mutable reference conversion
+/// between an outer enum's and its inner variant's type.
 pub trait AsVariantMut<T> {
+    /// Returns some mutable reference to the inner value if it is of type `T`, or `None` if it isn’t.
     fn as_variant_mut(&mut self) -> Option<&mut T>;
 }
 
+/// Used to do enum-to-variant conversions
+/// between an outer enum's and its inner variant's type.
 pub trait IntoVariant<T>: Sized {
+    /// Converts this type into the variant's type.
     fn into_variant(self) -> Result<T, Self>;
 }
 
+/// Convenience umbrella trait used to do a cheap
+///
+/// - reference-to-reference
+/// - mutable-to-mutable reference
+/// - reference-to-value
+/// - value-to-value
+///
+/// conversion between an outer enum's and its inner variant's type.
 pub trait VariantDowncast {
+    /// Returns some reference to the inner value if it is of type `T`, or `None`` if it isn’t.
     fn as_variant_downcast_ref<T>(&self) -> Option<&T>
     where
         Self: AsVariantRef<T>,
@@ -39,6 +65,7 @@ pub trait VariantDowncast {
         self.as_variant_ref()
     }
 
+    /// Returns some mutable reference to the inner value if it is of type `T`, or `None` if it isn’t.
     fn as_variant_downcast_mut<T>(&mut self) -> Option<&mut T>
     where
         Self: AsVariantMut<T>,
@@ -46,6 +73,7 @@ pub trait VariantDowncast {
         self.as_variant_mut()
     }
 
+    /// Returns a copy of the inner value if it is of type `T`, or `None`` if it isn’t.
     fn as_variant_downcast<T>(&self) -> Option<T>
     where
         T: Clone,
@@ -54,6 +82,7 @@ pub trait VariantDowncast {
         self.as_variant()
     }
 
+    /// Converts this type into the variant's type.
     fn into_variant_downcast<T>(self) -> Result<T, Self>
     where
         Self: IntoVariant<T>,
