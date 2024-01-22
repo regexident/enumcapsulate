74970ed8acb1859a54efcf36a066fc8bdf870ae0
Vincent Esche <regexident@gmail.com>
2024-01-22 02:14:24 +0100

    Rename '/tests/tests.rs' file to '/tests/derive-tests.rs'

--- enumcapsulate/tests/derive-tests.rs
+++ enumcapsulate/tests/derive-tests.rs
@@ -1,67 +1,67 @@
 mod from {
     #[test]
     pub fn pass() {
-        tryexpand::expand_checking(["tests/expand-macros/from/pass/**/*.rs"]);
+        tryexpand::expand(["tests/expand-macros/from/pass/**/*.rs"]).and_check();
     }
 
     #[test]
     pub fn fail() {
-        tryexpand::expand_fail(["tests/expand-macros/from/fail/**/*.rs"]);
+        tryexpand::expand(["tests/expand-macros/from/fail/**/*.rs"]).expect_fail();
     }
 }
 
 mod try_into {
     #[test]
     pub fn pass() {
-        tryexpand::expand_checking(["tests/expand-macros/try_into/pass/**/*.rs"]);
+        tryexpand::expand(["tests/expand-macros/try_into/pass/**/*.rs"]).and_check();
     }
 
     #[test]
     pub fn fail() {
-        tryexpand::expand_fail(["tests/expand-macros/try_into/fail/**/*.rs"]);
+        tryexpand::expand(["tests/expand-macros/try_into/fail/**/*.rs"]).expect_fail();
     }
 }
 
 mod from_variant {
     #[test]
     pub fn pass() {
-        tryexpand::expand_checking(["tests/expand-macros/from_variant/pass/**/*.rs"]);
+        tryexpand::expand(["tests/expand-macros/from_variant/pass/**/*.rs"]).and_check();
     }
 
     #[test]
     pub fn fail() {
-        tryexpand::expand_fail(["tests/expand-macros/from_variant/fail/**/*.rs"]);
+        tryexpand::expand(["tests/expand-macros/from_variant/fail/**/*.rs"]).expect_fail();
     }
 }
 
 mod as_variant_ref {
     #[test]
     pub fn pass() {
-        tryexpand::expand_checking(["tests/expand-macros/as_variant_ref/pass/**/*.rs"]);
+        tryexpand::expand(["tests/expand-macros/as_variant_ref/pass/**/*.rs"]).and_check();
     }
 
     #[test]
     pub fn fail() {
-        tryexpand::expand_fail(["tests/expand-macros/as_variant_ref/fail/**/*.rs"]);
+        tryexpand::expand(["tests/expand-macros/as_variant_ref/fail/**/*.rs"]).expect_fail();
     }
 }
 
 mod as_variant_mut {
     #[test]
     pub fn pass() {
-        tryexpand::expand_checking(["tests/expand-macros/as_variant_mut/pass/**/*.rs"]);
+        tryexpand::expand(["tests/expand-macros/as_variant_mut/pass/**/*.rs"]).and_check();
     }
 
     #[test]
     pub fn fail() {
-        tryexpand::expand_fail(["tests/expand-macros/as_variant_mut/fail/**/*.rs"]);
+        tryexpand::expand(["tests/expand-macros/as_variant_mut/fail/**/*.rs"]).expect_fail();
     }
 }
 
 mod as_variant {
     #[test]
     pub fn pass() {
-        tryexpand::expand_checking(["tests/expand-macros/as_variant/pass/**/*.rs"]);
+        tryexpand::expand(["tests/expand-macros/as_variant/pass/**/*.rs"]).and_check();
     }
 
     #[test]
@@ -77,31 +77,31 @@ mod as_variant {
 mod into_variant {
     #[test]
     pub fn pass() {
-        tryexpand::expand_checking(["tests/expand-macros/into_variant/pass/**/*.rs"]);
+        tryexpand::expand(["tests/expand-macros/into_variant/pass/**/*.rs"]).and_check();
     }
 
     #[test]
     pub fn fail() {
-        tryexpand::expand_fail(["tests/expand-macros/into_variant/fail/**/*.rs"]);
+        tryexpand::expand(["tests/expand-macros/into_variant/fail/**/*.rs"]).expect_fail();
     }
 }
 
 mod variant_downcast {
     #[test]
     pub fn pass() {
-        tryexpand::expand_checking(["tests/expand-macros/variant_downcast/pass/**/*.rs"]);
+        tryexpand::expand(["tests/expand-macros/variant_downcast/pass/**/*.rs"]).and_check();
     }
 
     #[test]
     pub fn fail() {
-        tryexpand::expand_fail(["tests/expand-macros/variant_downcast/fail/**/*.rs"]);
+        tryexpand::expand(["tests/expand-macros/variant_downcast/fail/**/*.rs"]).expect_fail();
     }
 }
 
 mod encapsulate {
     #[test]
     pub fn pass() {
-        tryexpand::expand_checking(["tests/expand-macros/encapsulate/pass/**/*.rs"]);
+        tryexpand::expand(["tests/expand-macros/encapsulate/pass/**/*.rs"]).and_check();
     }
 
     #[test]
